4b1ad54fc0eed8318ee3bf086f7b8f777c044412
art1shok <kvitkaart554@gmail.com>
2020-07-10 21:38:46 +0300

    added browser router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import './App.scss';
+import { BrowserRouter } from 'react-router-dom';
 import { store } from './store';
 import { RestaurantsListPage } from './components/RestaurantsListPage';
 import { Header } from './components/Header/Header';
@@ -8,13 +9,15 @@ import { Footer } from './components/Footer/Footer';
 
 const App = () => (
   <Provider store={store}>
-    <Header />
-    <main className="page">
-      <div className="content">
-        <RestaurantsListPage />
-      </div>
-    </main>
-    <Footer />
+    <BrowserRouter>
+      <Header />
+      <main className="page">
+        <div className="content">
+          <RestaurantsListPage />
+        </div>
+      </main>
+      <Footer />
+    </BrowserRouter>
   </Provider>
 );
 
