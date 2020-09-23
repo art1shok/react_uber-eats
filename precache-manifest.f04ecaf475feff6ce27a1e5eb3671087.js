b840bfbecf1a10ee3f2c4256b9c77caf291eaeb1
art1shok <kvitkaart554@gmail.com>
2020-09-23 14:56:42 +0300

    Updates

---
+++ precache-manifest.f04ecaf475feff6ce27a1e5eb3671087.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "810b38ba433e9728dc34af0897d14cd5",
+    "url": "/react_uber-eats/index.html"
+  },
+  {
+    "revision": "b3803d02efe8fd7c085d",
+    "url": "/react_uber-eats/static/css/main.1d5b7a93.chunk.css"
+  },
+  {
+    "revision": "f05c9b435985b0ad2a91",
+    "url": "/react_uber-eats/static/js/2.f17bd0ce.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react_uber-eats/static/js/2.f17bd0ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3803d02efe8fd7c085d",
+    "url": "/react_uber-eats/static/js/main.4f9e45ac.chunk.js"
+  },
+  {
+    "revision": "42aeb11b520e29194c26",
+    "url": "/react_uber-eats/static/js/runtime-main.2a02684a.js"
+  },
+  {
+    "revision": "45662d3d61a9963fbce63e3eb7ca1467",
+    "url": "/react_uber-eats/static/media/close.45662d3d.svg"
+  },
+  {
+    "revision": "7c048725670e6778b7c283e46363ec21",
+    "url": "/react_uber-eats/static/media/fb.7c048725.svg"
+  },
+  {
+    "revision": "4ef5487ca3ace946040e7efefbfd672d",
+    "url": "/react_uber-eats/static/media/ig.4ef5487c.svg"
+  },
+  {
+    "revision": "233253c0318b215ccb2b3f83670b1288",
+    "url": "/react_uber-eats/static/media/logo-white.233253c0.svg"
+  },
+  {
+    "revision": "3c398a8a1341ef1a48001f38f9049550",
+    "url": "/react_uber-eats/static/media/logo.3c398a8a.svg"
+  },
+  {
+    "revision": "c9bc8753bdd5a6ec128b1c9ed133eb78",
+    "url": "/react_uber-eats/static/media/place.c9bc8753.svg"
+  },
+  {
+    "revision": "810c92dfd823e8b78bfaf8401b31473b",
+    "url": "/react_uber-eats/static/media/searchicon.810c92df.svg"
+  },
+  {
+    "revision": "58a51e831e5a47ac3459caa2d7d111d8",
+    "url": "/react_uber-eats/static/media/tw.58a51e83.svg"
+  },
+  {
+    "revision": "e459d9a7586ae20abb20fbf159c2c982",
+    "url": "/react_uber-eats/static/media/world.e459d9a7.svg"
+  }
+]);
